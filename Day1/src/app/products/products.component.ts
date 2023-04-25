0f5be9f980bf623132b327d67b8ea3b2acad637b
Ibrahim Badr <hamedb.ibrahim@gmail.com>
2023-04-25 23:54:59 +0200

    Day 1 tasks done

--- Day1/src/app/products/products.component.ts
+++ Day1/src/app/products/products.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DiscountOffers } from '../Shared_Classes_&_types/DiscountOffers';
+import { ICategory } from '../Shared_Classes_&_types/ICategory';
+import { IProduct } from '../Shared_Classes_&_types/IProduct';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+
+export class ProductsComponent implements OnInit {
+  discount: DiscountOffers = DiscountOffers.TenPercent;
+  storeName: string = 'My Store';
+  storeLogo: string = '../assets/logo.png';
+  productList: IProduct[] = [
+    { id: 1, name: 'Product 1', quantity: 10, price: 20, img: '../assets/product1.jpg' },
+    { id: 2, name: 'Product 2', quantity: 5, price: 30, img: '../assets/product2.jpg' },
+    { id: 3, name: 'Product 3', quantity: 2, price: 50, img: '../assets/product3.jpg' }
+  ];
+  categoryList: ICategory[] = [
+    { id: 1, name: 'Category 1' },
+    { id: 2, name: 'Category 2' },
+    { id: 3, name: 'Category 3' }
+  ];
+  clientName: string = 'John Doe';
+  isPurchased: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
